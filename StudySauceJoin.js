3bad3f76cd67ebac520dc337b668319246395b8e
05kimsoonho <81752458+05kimsoonho@users.noreply.github.com>
2022-02-07 00:49:30 +0900

    Add files via upload

---
+++ StudySauceJoin.js
@@ -0,0 +1,98 @@
+// 휴대폰 번호 입력 부분
+function changePhone1(){
+    const phone1 = document.getElementById("phone1").value // 010
+    if(phone1.length === 3){
+        document.getElementById("phone2").focus();
+    }
+}
+function changePhone2(){
+    const phone2 = document.getElementById("phone2").value // 010
+    if(phone2.length === 4){
+        document.getElementById("phone3").focus();
+    }
+}
+function changePhone3(){
+    const phone3 = document.getElementById("phone3").value // 010
+    if(phone3.length === 4){
+      document.getElementById("sendMessage").focus();
+      document.getElementById("sendMessage").setAttribute("style","background-color:yellow;")
+      document.getElementById("sendMessage").disabled = false;
+    }
+}
+
+let processID = -1;
+
+// 가입부분 체크
+
+function signUpCheck(){
+
+  let id = document.getElementById("id").value
+  let name = document.getElementById("name").value
+  let password = document.getElementById("password").value
+  let passwordCheck = document.getElementById("passwordCheck").value
+  let area = document.getElementById("area").value
+  let gender_man = document.getElementById("gender_man").checked
+  let gender_woman = document.getElementById("gender_woman").checked
+  let check = true;
+
+  // 비밀번호 확인
+  if(password !== passwordCheck){
+    document.getElementById("passwordError").innerHTML=""
+    document.getElementById("passwordCheckError").innerHTML="비밀번호가 동일하지 않습니다."
+    check = false
+  }else{
+    document.getElementById("passwordError").innerHTML=""
+    document.getElementById("passwordCheckError").innerHTML=""
+  }
+
+  if(password===""){
+    document.getElementById("passwordError").innerHTML="비밀번호를 입력해주세요."
+    check = false
+  }else{
+    //document.getElementById("passwordError").innerHTML=""
+  }
+  if(passwordCheck===""){
+    document.getElementById("passwordCheckError").innerHTML="비밀번호를 다시 입력해주세요."
+    check = false
+  }else{
+    //document.getElementById("passwordCheckError").innerHTML=""
+  }
+
+  // 이름확인
+  if(name===""){
+    document.getElementById("nameError").innerHTML="이름이 올바르지 않습니다."
+    check = false
+  }else{
+    document.getElementById("nameError").innerHTML=""
+  }
+
+  // 지역선택 확인
+  if(area === "성별을 선택하세요."){
+    document.getElementById("areaError").innerHTML="성별을 선택해주세요."
+    check = false
+  }else{
+    document.getElementById("areaError").innerHTML=""
+  }
+
+  // 성별체크확인
+  if(!gender_man && !gender_woman){
+    document.getElementById("genderError").innerHTML="성별을 선택해주세요."
+    check = false
+  }else{
+    document.getElementById("genderError").innerHTML=""
+  }
+  
+  if(check){
+    document.getElementById("idError").innerHTML=""
+    document.getElementById("passwordError").innerHTML=""
+    document.getElementById("passwordCheckError").innerHTML=""
+    document.getElementById("nameError").innerHTML=""
+    document.getElementById("areaError").innerHTML=""
+    document.getElementById("genderError").innerHTML=""
+    
+    //비동기 처리이벤트
+    setTimeout(function() {
+      alert("가입이 완료되었습니다.")
+  },0);
+  }
+}
